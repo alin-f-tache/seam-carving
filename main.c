ae1468afcc37369ae2d2282b4d5a9aa187f120ff
alin-f-tache <alin.f.tache@gmail.com>
2019-11-26 08:45:20 +0200

    split into functions

--- main.c
+++ main.c
@@ -12,18 +12,19 @@ typedef struct {
 	unsigned char b;
 } pixel;
 
-long long min2(long long l1, long long l2) {
+long min2(long l1, long l2) {
 	if (l1 < l2)
 		return l1;
 	return l2;
 }
 
-long long min3(long long l1, long long l2, long long l3) {
+long min3(long l1, long l2, long l3) {
 	return min2(l1, min2(l2, l3));
 }
 
-long long dual_gradient_energy(pixel p1x, pixel p2x, pixel p1y, pixel p2y) {
+long dual_gradient_energy(pixel p1x, pixel p2x, pixel p1y, pixel p2y) {
 	/*
+	===============================================================================================
 	Functie care calculeaza energia unui pixel in functie de vecinii sai:
 	p1x, p2x - vecinii din stanga si din dreapta sa
 	p1y, p2y - vecinii de deasupra si de dedesubtul sau
@@ -38,10 +39,121 @@ long long dual_gradient_energy(pixel p1x, pixel p2x, pixel p1y, pixel p2y) {
 	ydif.g = abs(p1y.g - p2y.g);
 	ydif.b = abs(p1y.b - p2y.b);
 
-	long long xgrad = xdif.r * xdif.r + xdif.g * xdif.g + xdif.b * xdif.b;
-	long long ygrad = ydif.r * ydif.r + ydif.g * ydif.g + ydif.b * ydif.b;
+	long xgrad = xdif.r * xdif.r + xdif.g * xdif.g + xdif.b * xdif.b;
+	long ygrad = ydif.r * ydif.r + ydif.g * ydif.g + ydif.b * ydif.b;
 
 	return xgrad + ygrad;
+}
+
+long **generate_energy_matrix(int h, int w, pixel **color_mat) {
+	/*
+	===============================================================================================
+	Functie care genereaza matricea cu energiile fiecarui pixel
+	*/
+	int i, j;
+	long **energy_matrix = malloc(h * sizeof(long *));
+	for (i = 0; i < h; i++)
+		energy_matrix[i] = malloc(w * sizeof(long));
+
+	for (i = 0; i < h; i++)
+		for (j = 0; j < w; j++) {
+			long energy;
+			/* 
+			Colturi
+			*/
+			if (i == 0 && j == 0) {
+				energy = dual_gradient_energy(color_mat[i][w - 1], color_mat[i][j + 1], color_mat[h - 1][j], color_mat[i + 1][j]);
+			}
+			else if (i == 0 && j == w - 1)
+				energy = dual_gradient_energy(color_mat[i][j - 1], color_mat[i][0], color_mat[h - 1][j], color_mat[i + 1][j]);
+			else if (i == h - 1 && j == 0)
+				energy = dual_gradient_energy(color_mat[i][w - 1], color_mat[i][j + 1], color_mat[i - 1][j], color_mat[0][j]);
+			else if (i == h - 1 && j == w - 1)
+				energy = dual_gradient_energy(color_mat[i][j - 1], color_mat[i][0], color_mat[i - 1][j], color_mat[0][j]);
+			/*
+			Margini
+			*/
+			else if (i == 0)
+				energy = dual_gradient_energy(color_mat[i][j - 1], color_mat[i][j + 1], color_mat[h - 1][j], color_mat[i + 1][j]);
+			else if (i == h - 1)
+				energy = dual_gradient_energy(color_mat[i][j - 1], color_mat[i][j + 1], color_mat[i - 1][j], color_mat[0][j]);
+			else if (j == 0)
+				energy = dual_gradient_energy(color_mat[i][w - 1], color_mat[i][j + 1], color_mat[i - 1][j], color_mat[i + 1][j]);
+			else if (j == w - 1)
+				energy = dual_gradient_energy(color_mat[i][j - 1], color_mat[i][0], color_mat[i - 1][j], color_mat[i + 1][j]);
+			/*
+			Interior
+			*/
+			else
+				energy = dual_gradient_energy(color_mat[i][j - 1], color_mat[i][j + 1], color_mat[i - 1][j], color_mat[i + 1][j]);
+			energy_matrix[i][j] = energy;
+		}
+	return energy_matrix;
+}
+
+long **generate_seam_energies(int h, int w, long **energy_matrix) {
+	/*
+	===============================================================================================
+	Functie care genereaza energiile tuturor seam-urilor prin programare dinamica
+	*/
+	int i, j;
+	long **dp = malloc(h * sizeof(long *));
+	for (i = 0; i < h; i++)
+		dp[i] = malloc(w * sizeof(long));
+
+	for (i = 1; i < h; i++)
+		for (j = 0; j < w; j++)
+			if (j == 0)
+				dp[i][j] = energy_matrix[i][j] + min2(dp[i - 1][j], dp[i - 1][j + 1]);
+			else if (j == w - 1)
+				dp[i][j] = energy_matrix[i][j] + min2(dp[i - 1][j - 1], dp[i - 1][j]);
+			else
+				dp[i][j] = energy_matrix[i][j] + min3(dp[i - 1][j - 1], dp[i - 1][j], dp[i - 1][j + 1]);
+
+	return dp;
+}
+
+int *determine_min_seam(int h, int w, long **dp) {
+	/*
+	===============================================================================================
+	Functie care determina seam-ul minim ce trebuie sters din imagine
+	*/
+	int i, j;
+	int *vertical_seam = malloc(h * sizeof(int));
+	long min_energy = 2000000;
+
+	min_energy = dp[h - 1][0];
+	for (j = 0; j < w; j++)
+		if (dp[h - 1][j] < min_energy) {
+			min_energy = dp[h - 1][j];
+			vertical_seam[h - 1] = j;
+		}
+
+	for (i = h - 2; i >= 0; i--) {
+		if (vertical_seam[i + 1] == 0) {
+			if (dp[i][vertical_seam[i + 1]] < dp[i][vertical_seam[i + 1] + 1]) {
+				vertical_seam[i] = vertical_seam[i + 1];
+			} else {
+				vertical_seam[i] = vertical_seam[i + 1] + 1;
+			}
+		} else if (vertical_seam[i + 1] == w - 1) {
+			if (dp[i][vertical_seam[i + 1] - 1] < dp[i][vertical_seam[i + 1]]) {
+				vertical_seam[i] = vertical_seam[i + 1] - 1;
+			} else {
+				vertical_seam[i] = vertical_seam[i + 1];
+			}
+		} else {
+			if (dp[i][vertical_seam[i + 1] - 1] < dp[i][vertical_seam[i + 1]] && dp[i][vertical_seam[i + 1] - 1] < dp[i][vertical_seam[i + 1] + 1]) {
+				vertical_seam[i] = vertical_seam[i + 1] - 1;
+			} else if (dp[i][vertical_seam[i + 1]] < dp[i][vertical_seam[i + 1] + 1]) {
+				vertical_seam[i] = vertical_seam[i + 1];
+			} else {
+				vertical_seam[i] = vertical_seam[i + 1] + 1;
+			}
+		}
+	}
+
+	return vertical_seam;
 }
 
 int main(int argc, char* argv[]) {
@@ -79,109 +191,25 @@ int main(int argc, char* argv[]) {
 			/*
 			=======================================================================================
 			Calculam energia fiecarui pixel
-			*/
-			long **energy_matrix = malloc(h * sizeof(long *));
-			for (i = 0; i < h; i++)
-				energy_matrix[i] = malloc(w * sizeof(long));
-
-			for (i = 0; i < h; i++)
-				for (j = 0; j < w; j++) {
-					long energy;
-					/* 
-					Colturi
-					*/
-					if (i == 0 && j == 0) {
-						energy = dual_gradient_energy(color_mat[i][w - 1], color_mat[i][j + 1], color_mat[h - 1][j], color_mat[i + 1][j]);
-					}
-					else if (i == 0 && j == w - 1)
-						energy = dual_gradient_energy(color_mat[i][j - 1], color_mat[i][0], color_mat[h - 1][j], color_mat[i + 1][j]);
-					else if (i == h - 1 && j == 0)
-						energy = dual_gradient_energy(color_mat[i][w - 1], color_mat[i][j + 1], color_mat[i - 1][j], color_mat[0][j]);
-					else if (i == h - 1 && j == w - 1)
-						energy = dual_gradient_energy(color_mat[i][j - 1], color_mat[i][0], color_mat[i - 1][j], color_mat[0][j]);
-					/*
-					Margini
-					*/
-					else if (i == 0)
-						energy = dual_gradient_energy(color_mat[i][j - 1], color_mat[i][j + 1], color_mat[h - 1][j], color_mat[i + 1][j]);
-					else if (i == h - 1)
-						energy = dual_gradient_energy(color_mat[i][j - 1], color_mat[i][j + 1], color_mat[i - 1][j], color_mat[0][j]);
-					else if (j == 0)
-						energy = dual_gradient_energy(color_mat[i][w - 1], color_mat[i][j + 1], color_mat[i - 1][j], color_mat[i + 1][j]);
-					else if (j == w - 1)
-						energy = dual_gradient_energy(color_mat[i][j - 1], color_mat[i][0], color_mat[i - 1][j], color_mat[i + 1][j]);
-					/*
-					Interior
-					*/
-					else
-						energy = dual_gradient_energy(color_mat[i][j - 1], color_mat[i][j + 1], color_mat[i - 1][j], color_mat[i + 1][j]);
-					energy_matrix[i][j] = energy;
-				}
-
+			*/			
+			long **energy_matrix = generate_energy_matrix(h, w, color_mat);
 
 			/*
 			=======================================================================================
 			Determinam energiile seam-urilor prin programare dinamica
 			*/
-			long **dp = malloc(h * sizeof(long *));
-			for (i = 0; i < h; i++)
-				dp[i] = malloc(w * sizeof(long));
-			int vertical_seam[h];
-			long min_energy;
-
-			for (i = 1; i < h; i++)
-				for (j = 0; j < w; j++)
-					if (j == 0)
-						dp[i][j] = energy_matrix[i][j] + min2(dp[i - 1][j], dp[i - 1][j + 1]);
-					else if (j == w - 1)
-						dp[i][j] = energy_matrix[i][j] + min2(dp[i - 1][j - 1], dp[i - 1][j]);
-					else
-						dp[i][j] = energy_matrix[i][j] + min3(dp[i - 1][j - 1], dp[i - 1][j], dp[i - 1][j + 1]);
+			long **dp = generate_seam_energies(h, w, energy_matrix);
 
 			/*
 			=======================================================================================
 			Determinam seam-ul vertical ce trebuie eliminat
 			*/
-			min_energy = dp[h - 1][0];
-			for (j = 0; j < w; j++)
-				if (dp[h - 1][j] < min_energy) {
-					min_energy = dp[h - 1][j];
-					vertical_seam[h - 1] = j;
-				}
-
-			for (i = h - 2; i >= 0; i--) {
-				long remaining = dp[i + 1][vertical_seam[i + 1]] - energy_matrix[i + 1][vertical_seam[i + 1]];
-				if (vertical_seam[i + 1] == 0) {
-					if (dp[i][vertical_seam[i + 1]] == remaining) {
-						vertical_seam[i] = vertical_seam[i + 1];
-					} else if (dp[i][vertical_seam[i + 1] + 1] == remaining) {
-						vertical_seam[i] = vertical_seam[i + 1] + 1;
-					}
-				} else if (vertical_seam[i + 1] == w - 1) {
-					if (dp[i][vertical_seam[i + 1] - 1] == remaining) {
-						vertical_seam[i] = vertical_seam[i + 1] - 1;
-					} else if (dp[i][vertical_seam[i + 1]] == remaining) {
-						vertical_seam[i] = vertical_seam[i + 1];
-					}
-				} else {
-					if (dp[i][vertical_seam[i + 1] - 1] == remaining) {
-						vertical_seam[i] = vertical_seam[i + 1] - 1;
-					} else if (dp[i][vertical_seam[i + 1]] == remaining) {
-						vertical_seam[i] = vertical_seam[i + 1];
-					} else if (dp[i][vertical_seam[i + 1] + 1] == remaining) {
-						vertical_seam[i] = vertical_seam[i + 1] + 1;
-					}
-				}
-			}
+			int *vertical_seam = determine_min_seam(h, w, dp);
 
 			/*
 			=======================================================================================
 			Eliminam seam-ul vertical si scadem latimea imaginii
 			*/
-			/*for (i = 0; i < h; i++)
-				printf("%d ", vertical_seam[i]);
-			printf("\n");*/
-
 			for (i = 0; i < h; i++) {
 				pixel *new_line = malloc((w - 1) * sizeof(pixel));
 				int skipped = 0;
@@ -196,6 +224,11 @@ int main(int argc, char* argv[]) {
 			}
 			w -= 1;
 
+			/*
+			=======================================================================================
+			Clean-up
+			*/
+			free(vertical_seam);
 			for (i = 0; i < h; i++) {
 				free(energy_matrix[i]);
 				free(dp[i]);
